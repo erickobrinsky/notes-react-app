516fd17118a9f0f70a574f8e68127f504f72ad4a
Eric Kobrinsky <78773449+erickobrinsky@users.noreply.github.com>
2021-10-10 14:54:39 +0300

    text library resize

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import {MdDeleteForever} from 'react-icons/md'
+import autoLineBreak  from 'auto-line-breaks';
 
 const Note = ({note, handleDeleteNote}) => {
     const { id, text, date, title } = note
+    const lineLength = 15
+    const result = autoLineBreak(text, lineLength)
     return (  
         <div className="note">
             
             <h2>{(title.length > 0) ? `Title: ${title}` : ''}</h2>
-            <p>{text}</p>
+            <p>{result}</p>
             <div className="note-footer">
                 <small>{date}</small>
                 <MdDeleteForever className="delete-icon" size="1.3em"
